55291cbacf12540f6d30d4d8641217c6fb75cdaa
RedCony <blackxism@gmail.com>
2022-01-17 14:51:50 +0900

    벡터2 마그니튜트가 길이를 구하는 것이다.

--- Hello World/Assets/Scripts/App.cs
+++ Hello World/Assets/Scripts/App.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class App : MonoBehaviour
 {
+    public int damage;
+    public float hp;
+    public string myName;
+    public bool isDie;
+
+    public string[] arrNames;
+    public GameObject playerGo;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hello world");
+        //Debug.Log("hello world");
+        Vector2 stratpos = new Vector2(2.0f,1.0f);
+        Vector2 endpos = new Vector2(8.0f, 5.0f);
+        Vector2 dir = endpos - stratpos;
+        Debug.Log(dir);
+        float distance = dir.magnitude;
+        Debug.Log(distance);
     }
 
     // Update is called once per frame
