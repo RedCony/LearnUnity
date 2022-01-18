9b4c24b8b812dae56c61e90f653441281fcdead6
RedCony <blackxism@gmail.com>
2022-01-18 18:27:25 +0900

    player 이동 제한 하기 화면 밖으로 안나가게 하려는 중임

--- CatEscape/Assets/Scripts/PlayerController.cs
+++ CatEscape/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float _rightBoundary = 0f;
+    [SerializeField] private float _leftBoundary = 0f;
+    [SerializeField] private float _speed = 3.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             this.transform.Translate(-3, 0, 0, Space.World);
@@ -26,11 +32,27 @@ public class PlayerController : MonoBehaviour
 
     public void LButtonDown()
     {
-        transform.Translate(-3, 0, 0, Space.World);
+       
+       
+        
     }
 
     public void RButtonDown()
     {
-        transform.Translate(3, 0, 0, Space.World);
+        transform.Translate(_speed, 0, 0, Space.World);
+    }
+
+    public void Movement()
+    {
+        Boundary();
+        float _horizontalInput = Input.GetAxisRaw("Horizontal");
+        Vector3 _direction = new Vector3(_horizontalInput, 0, 0).normalized;
+        transform.Translate(_direction * _speed * Time.deltaTime, 0, 0);
+    }
+
+    public void Boundary()
+    {
+        float _xMovementClamp = Mathf.Clamp(transform.position.x, _leftBoundary, _rightBoundary);
+        transform.position = new Vector3(_xMovementClamp, 0, 0);
     }
 }
