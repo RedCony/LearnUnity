ed94c81ba81c247d17201a81e89c5c19c68b4fcd
RedCony <blackxism@gmail.com>
2022-01-19 18:06:43 +0900

    player 화면 밖으로 나가지 못하게 하기 완료 Mathf.Clamp 사용 하였음

--- CatEscape3/Assets/Scripts/PlayerController.cs
+++ CatEscape3/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    public float speed=1;
+    public Transform leftBoundaryPoint;
+    public Transform rightBoundaryPoint;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +20,20 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             this.transform.Translate(Vector2.left * this.speed);
+            CheckBoundary();
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             this.transform.Translate(Vector2.right * this.speed);
+            CheckBoundary();
         }
         
+    }
+    private void CheckBoundary()
+    {
+        var pos = this.transform.position;
+        pos.x = Mathf.Clamp(this.transform.position.x, this.leftBoundaryPoint.position.x, this.rightBoundaryPoint.position.x);
+        this.transform.position = pos;
+
     }
 }
