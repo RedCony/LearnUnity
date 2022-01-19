e904fed7a9e002501849df0617cde61b8d61a490
RedCony <blackxism@gmail.com>
2022-01-19 12:08:32 +0900

    Mathf.Clamp를 적용해서 좌우로 화면에서 빠지는 걸 제어 하였습니다.

--- CatEscape2/Assets/Scripts/PlayerController.cs
+++ CatEscape2/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public Transform leftBoundaryPoint;
+    public Transform rightBoundaryPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +19,19 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             this.transform.Translate(Vector2.left* this.speed);
+            CheckBoundary();
+           
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             this.transform.Translate(Vector2.right * this.speed);
+            CheckBoundary();
         }
+    }
+    private void CheckBoundary()
+    {
+        var pos = this.transform.position;
+        pos.x = Mathf.Clamp(this.transform.position.x, this.leftBoundaryPoint.position.x, this.rightBoundaryPoint.position.x);
+        this.transform.position = pos;
     }
 }
