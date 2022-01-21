bd92639f0d8b189660823303252f328222cf0d5b
RedCony <68505636+RedCony@users.noreply.github.com>
2022-01-22 01:31:46 +0900

    룰렛 돌리기 2초동안 돌다가 3초 뒤부터 감속되는 것을 구현 하려고 합니다. 검색해서 대입해 보았으나 원하는 대로 나오진 않았습니다.  조금더 검색 하고 검증을 해보고 대입을 해보도록 하겠습니다.

--- Roulette/Assets/Scripts/RouletteController.cs
+++ Roulette/Assets/Scripts/RouletteController.cs
@@ -8,29 +8,61 @@ public class RouletteController : MonoBehaviour
     {
         LEFT = 1, RIGHT = -1
     }
-    public float deceleration;
+    public float deceleration =0.96f;
+
 
     public eDir dir;
-    public float initRotSpeed = 10;
-    float rotSpeed = 0;
+    public float initRotSpeed = 100.0f;
+    float rotSpeed ;
+    float timeSpan;
+    float checkTime;
+    
     // Start is called before the first frame update
     void Start()
     {
-       // this.rotSpeed = 10;
+        // this.rotSpeed = 10;
+        timeSpan = 0.0f;
+        checkTime = 3.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
         if (Input.GetMouseButtonDown(0))
         {
-            rotSpeed =this.initRotSpeed * (int)dir;
+            rotSpeed = this.initRotSpeed * (int)dir;
+           
         }
+
+        timeSpan += Time.deltaTime;
+        if (timeSpan > checkTime)
+        {
+
+            this.transform.Rotate(0, 0, this.rotSpeed);
+            timeSpan = 0;
+
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            this.rotSpeed *= this.deceleration;
+        }
+         
         //회전 속도 만큰 룰렛을 회전 시킨다.
-        this.transform.Rotate(0, 0, this.rotSpeed);
+        
 
         //룰렛 속도를 감속시킨다.
+        
+      
+        
+
+        //Pause();
+    }
+
+    private void Pause()
+    {
+        //Time.timeScale = (float)3;
         this.rotSpeed *= this.deceleration;
     }
 }
