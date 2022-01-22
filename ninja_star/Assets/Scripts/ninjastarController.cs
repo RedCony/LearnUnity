dee4bace0f41a4215ca363ebbe0d1118afcaf2ee
RedCony <68505636+RedCony@users.noreply.github.com>
2022-01-22 17:03:59 +0900

    현재는 따라가기에 바빠서 하지 못하지만 어느 정도 여유를 찾게 되면 다시 시도해 보도록 하겠습니다.

--- ninja_star/Assets/Scripts/ninjastarController.cs
+++ ninja_star/Assets/Scripts/ninjastarController.cs
@@ -7,6 +7,7 @@ public class ninjastarController : MonoBehaviour
     Vector2 startpos;
     Vector2 endpos;
     float rotSpeed;
+    float speed;
     
 
 
@@ -19,11 +20,21 @@ public class ninjastarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.startpos = Input.mousePosition;
+            
+            rotSpeed = 10f;
+        }
+        transform.Rotate(0, 0, rotSpeed , Space.World);
         if (Input.GetMouseButtonUp(0))
         {
-            rotSpeed = 10f;
-            transform.Translate(Vector2.up * Time.deltaTime,Space.World);
-            
+            var endpos = Input.mousePosition;
+            float swipeLength = endpos.x - this.startpos.x;
+            this.speed = swipeLength / 500.0f;
+
         }
+       
+        transform.Translate(Vector2.up, Space.World);
     }
 }
