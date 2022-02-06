b46702b3d58a4290fd751271a66c739af2656351
RedCony <68505636+RedCony@users.noreply.github.com>
2022-02-06 23:33:54 +0900

    아이템정보창 띄우기 도전 중 버튼 클릭이 되지 않음

--- LearnUI/Assets/Scripts/UIItem.cs
+++ LearnUI/Assets/Scripts/UIItem.cs
@@ -7,9 +7,18 @@ public class UIItem : MonoBehaviour
 {
     public Image icon;
     public Text count;
-    
-    
+    public Button btninfo;
 
+    public UIItemInfo UIItemInfo;
+
+
+    void Start()
+    {
+        //this.UIItemInfo = FindObjectOfType("UIItemInfo");
+        this.btninfo.onClick.AddListener(() => {
+            UIItemInfo.Open();
+        });
+    }
     public void Init(Sprite sp,ItemInfo itemInfo)
     {
         if (sp == null)
@@ -22,7 +31,7 @@ public class UIItem : MonoBehaviour
             icon.SetNativeSize();
             this.icon.gameObject.SetActive(true);
             this.count.text = itemInfo.count.ToString();
-               
+          
         }
     }
 }
---
+++ LearnUI/Assets/Scripts/UIItemInfo.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIItemInfo : MonoBehaviour
+{
+    public UIItem uIItem;
+
+    public Image image;
+
+    public Text name;
+    public Text description1;
+    public Text description2;
+    public Text description3;
+
+    public void Init(string name,string spritename)
+    {
+        this.image = uIItem.GetComponent<Image>();
+
+        this.name.text = name.ToString();
+        this.description1.text = spritename.ToString();
+    }
+
+    public void Open()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void Close()
+    {
+        this.gameObject.SetActive(false);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.image = uIItem.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
